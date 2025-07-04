412203ffa27dc073f5496dfa42644d1feb52b547
TelaniWalakanda <wkitelani@gmail.com>
2025-07-04 16:07:37 +0530

    Change color of the text

--- src/components/common/TwoColorHeaderWithDescription.tsx
+++ src/components/common/TwoColorHeaderWithDescription.tsx
@@ -11,9 +11,9 @@ export default function HeaderWithDescription({
 }: HeaderWithDescriptionProps) {
     return (
         <div className='text-center md:max-w-[70%] mx-auto'>
-            <h2 className='uppercase text-3xl'>
+            <h2 className='uppercase text-3xl lg:text-5xl'>
                 <span className='block'>{titleThin}</span>
-                <span className='block font-bold text-secondary'>{titleBold}</span>
+                <span className='block font-bold text-primary'>{titleBold}</span>
             </h2>
             <p className='my-10'>{description}</p>
         </div>
